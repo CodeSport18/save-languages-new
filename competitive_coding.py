638a1549c355232aa1d5c061f2cdc484177f014b
Neel <neelvraina@gmail.com>
2025-06-19 17:46:38 -0700

    Add image preview functionality to create lesson form

--- competitive_coding.py
+++ competitive_coding.py
@@ -61,3 +61,71 @@
 
 # for output in outputs:
 #     print(output)
+
+
+
+
+
+
+
+
+# https://usaco.org/index.php?page=viewproblem2&cpid=1088
+
+# n = int(input())
+
+# grid_by_rows = [list(map(int, input().split())) for _ in range(n)]
+
+# grid_by_cols = zip(*grid_by_rows)
+
+# rows_alternate = 0
+# columns_alternate = 0
+
+# for i in range(n):
+
+# 	ith_row = grid_by_rows[i]
+
+# 	rows_alternate += max(sum(ith_row[::2]), sum(ith_row[1::2]))
+
+# 	ith_col = next(grid_by_cols)
+# 	columns_alternate += max(sum(ith_col[::2]), sum(ith_col[1::2]))
+
+# print(max(rows_alternate, columns_alternate))
+
+
+
+
+
+
+
+
+# https://usaco.org/index.php?page=viewproblem2&cpid=1491
+
+grid_size, updates = (int(x) for x in input().split())
+original_grid = [[x == '#' for x in input()] for asdf in range(grid_size)]
+
+def getanswer(original_grid):
+  
+  best = grid_size*grid_size
+  
+  for grid in range(2 ** ((grid_size**2)//4)):
+    wrongcount = 0
+
+    for i in range(grid_size//2+1):
+      for j in range(grid_size//2+1):
+
+        truei = min(i, grid_size-1-i)
+        truej = min(j, grid_size-1-j)
+
+        if original_grid[i][j] != ((grid & (2 ** (truei * (grid_size // 2) + truej))) != 0):
+          wrongcount += 1
+
+    best = min(best, wrongcount)
+
+  return best
+
+print(getanswer(original_grid))
+
+for asdf in range(updates):
+  a, b = (int(x)-1 for x in input().split())
+  original_grid[a][b] = not original_grid[a][b]
+  print(getanswer(original_grid))
